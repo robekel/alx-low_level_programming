74bc187210ed4430cce2cbea1a3e471a9b9da51d
Robel Ketema <robell.ketema18@gmail.com>
2022-03-16 16:36:26 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @str: character array
+ * 
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return 0;
+}
