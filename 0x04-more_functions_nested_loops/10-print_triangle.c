7e8d7de821767a81944fd41ecc09000c5723c497
Robel Ketema <robell.ketema@gmail.com>
2022-03-20 19:35:47 +0000

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - print numbers 0-9.
+ * @n: number of times to print
+ *
+ * Return: numbers.
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+	int tmp = size;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = tmp ; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+
+			for(k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			tmp -= 1;
+			_putchar('\n');
+		}
+
+	}
+}
