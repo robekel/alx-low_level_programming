ebe0249a8186b1bd287b585e70bc07c251c33ca3
Robel Ketema <robell.ketema@gmail.com>
2022-03-21 11:24:48 +0000

    11. The largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - find the largest prime factor
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+	long int n;
+	long int div=2;
+	long int maxFact;
+
+	n = 612852475143;
+
+	while (n != 0)
+	{
+		if ((n % div) != 0)
+		{
+			div = div + 1;
+		}
+	        else
+	        {
+	            	maxFact = n;
+	            	n = n / div;
+
+	            	if (n == 1)
+	            	{
+	            		printf("%ld\n", maxFact);
+	            	        break;
+	            	}
+	        }
+	}
+	return (0);
+}
