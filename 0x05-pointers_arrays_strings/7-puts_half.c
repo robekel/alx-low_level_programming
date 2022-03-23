95b22c925452c15ccbe957d27e8d3b6de2e31237
Robel Ketema <robell.ketema@gmail.com>
2022-03-23 06:40:50 +0000

    7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * puts_half - reverse string
+ * @str: string to be reversed
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int len;
+	int n;
+
+	i = 0;
+	len = 0;
+	while (str[i] != '\0')
+	{
+		len += 1;
+		i++;
+	}
+
+	if (len % 2 != 0)
+	{
+		n = (len - 1) / 2;
+		for (i = n; i <= len; i++)
+		{
+			_putchar(str[i]);
+			_putchar(str[len]);
+		}
+	}
+	else
+	{
+		n = len /2;
+		for (i = n; i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	_putchar('\n');
+}
