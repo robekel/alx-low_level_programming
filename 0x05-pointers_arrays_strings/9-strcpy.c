41117af25b72cdbe9e2f9ac105274e9fd4e5a766
Robel Ketema <robell.ketema@gmail.com>
2022-03-23 08:36:13 +0000

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcpy - print a string in reverse.
+ * @dest: buffer
+ * @src: to be copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (src[i] != '\0')
+	{
+		len += 1;
+		i++;
+	}
+	
+	for (j = 0; j <= len; j++)
+	{
+		dest[j] = src[j];
+	}
+	return dest;
+}
