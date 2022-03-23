18154689b905b6ccae03a076db0af6ec39288357
Robel Ketema <robell.ketema@gmail.com>
2022-03-23 06:10:53 +0000

    6. Half the lies they tell about me arent true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,20 +8,11 @@
 void puts2(char *str)
 {
 	int i;
-	int j;
-	int len;
-
-	len = 0;
-
-	for (j = 0; str[j] != '\0'; j++)
-	{
-		len += 1;
-	}
 	
-	for (i = 0; i < len / 2; i++)
+	for (i = 0; i != '\0' / 2; i++)
 	{
 		if (i % 2 == 0)
-			_putchar(str[i * 2]);
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
