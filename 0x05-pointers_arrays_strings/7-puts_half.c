2af73dbfcd4fbb7e9d1fc1f044291c8d25ed662a
Robel Ketema <robell.ketema@gmail.com>
2022-03-23 06:43:30 +0000

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,12 +25,11 @@ void puts_half(char *str)
 		for (i = n; i <= len; i++)
 		{
 			_putchar(str[i]);
-			_putchar(str[len]);
 		}
 	}
 	else
 	{
-		n = len /2;
+		n = len / 2;
 		for (i = n; i <= len; i++)
 		{
 			_putchar(str[i]);
