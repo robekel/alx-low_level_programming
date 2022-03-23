21a1b55eaddfa09c0bea24f708ae5af0d015fe4f
Robel Ketema <robell.ketema@gmail.com>
2022-03-23 05:59:37 +0000

    6. Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - reverse string
+ * @str: string to be reversed
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j;
+	int len;
+
+	len = 0;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		len += 1;
+	}
+	
+	for (i = 0; i < len/2; i++)
+	{
+		if (i == 0)
+			_putchar(str[0]);
+
+		else
+			_putchar(str[i * 2]);
+	}
+	_putchar('\n');
+}
