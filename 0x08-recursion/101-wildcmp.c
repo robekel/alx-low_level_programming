ce4e666ce86273676fff636630b268f638d7bf9a
Robel Ketema <robell.ketema@gmail.com>
2022-03-31 21:14:32 +0000

    7. string compare

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * factorial - prints the factrial of a given number
- * @n: input number
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: -1 if error
+ * Return: 1 if palindrom 0 if not
  */
 
-int factorial(int n)
+int wildcmp(char *s1, char *s2)
 {
-	if (n < 0)
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
 	{
-		return (-1);
-	}
-	else
-	{
-		if (n == 0)
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
-		return (n * factorial(n - 1));
 	}
 }
