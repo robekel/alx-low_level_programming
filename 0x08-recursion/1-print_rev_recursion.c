40f513784c013520c737e77f693e5e1131831f4b
Robel Ketema <robell.ketema@gmail.com>
2022-03-31 01:21:42 +0000

    0. string rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string followed by a new line
+ * @s: input string
+ *
+ * Return -  1
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
