6f6e1b8fe18c1e38aaea190b3d9340a3a2f89466
Robel Ketema <robell.ketema@gmail.com>
2022-04-06 01:58:02 +0000

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
  *
  * Return: Pointer
  */
+
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
@@ -22,11 +23,9 @@ char *create_array(unsigned int size, char c)
 
 	if (s == 0)
 		return (0);
-
 	for (i = 0; i < size; i++)
 		*(s + i) = c;
 
 	*(s + i) = '\0';
-
 	return (s);
 }
