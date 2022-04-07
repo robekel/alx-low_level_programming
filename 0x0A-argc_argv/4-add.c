7611ac079c7c4dc0bb7805c31bca0190907d2229
Robel Ketema <robell.ketema@gmail.com>
2022-04-07 02:13:36 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds numbers
+ * @argc: the number of passed arguments
+ * @argv: an array of passsed arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int _isnumber(char *s)
+{
+	int i, check, d;
+
+	i = 0, d = 0, check = 1;
+
+	if (*s == '-')
+		i++;
+	for (; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
+}
 
 /**
  * main - adds numbers
@@ -12,26 +41,24 @@
 
 int main(int argc, char  **argv)
 {
-	int i;
-	int sum = 0;
+	int i, n, ex;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+	ex = 0, n = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		if (atoi(argv[i]) == 0)
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (_isnumber(argv[i]))
+				n += atoi(argv[i]);
+			else
+				ex = 1;
 		}
-
-		sum = sum + atoi(argv[i]);
 	}
 
-	printf("%d\n", sum);
-	return (0);
+	if (ex == 0)
+		printf("%i\n", n);
+	else
+		printf("%s\n", "Error");
+	return (ex);
 }
