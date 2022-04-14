1fc772b6306064b804f4114da2c5fb35a9cd9054
Robel Ketema <robell.ketema@gmail.com>
2022-04-14 23:49:33 +0000

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,20 +14,24 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 	int v;
+	int counter = 0;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		v = cmp(array[i]);
+		counter += v;
 		if (v != 0)
 		{
 			return (i);
 			break;
 		}
 	}
-	if (i == size - 1)
+	if (counter == 0)
 		return (-1);
 	return (0);
 }
