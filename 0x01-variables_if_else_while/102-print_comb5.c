ff20f3d84bd3817f82e520a55cc7a477bc84ffb3
Robel Ketema <robell.ketema@gmail.com>
2022-04-19 17:48:52 +0000

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: int
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					if ((i + j) >= (k + l))
+						continue;
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
