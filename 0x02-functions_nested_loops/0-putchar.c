b591277214b46693d07aafa17641bacc792b0ad3
Robel Ketema <robell.ketema@gmail.com>
2022-04-20 17:52:58 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Return: o
+ */
+int main()
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
